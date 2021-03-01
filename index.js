d4311571b36776f989a0e322ca515e0d52e6daea
Young Won <youngwon@Youngs-MacBook-Pro.local>
2021-02-28 23:05:07 -0800

    random color fn added

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-function othername() {
-  var input = document.getElementById('userInput').value;
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-  alert(input);
-  document.write(`<h1>My name is ${input}</h1>`);
+function othername() {
+  var inputEle = document.getElementById('userInput');
+  var input = inputEle.value;
+  var myint = parseInt(input);
+  var mydoc = document.querySelector('.answer');
+  // var mynode = document.createElement('div');
+  for (var i = 0; i < myint; i++) {
+    var myitem = mydoc.appendChild(document.createElement('div'));
+    myitem.style.color = getRandomColor();
+    myitem.textContent = i;
+    inputEle.value = '';
+  }
+
+  // alert(input);
+  // document.write(`<h1>My name is ${input}</h1>`);
+
+  console.log(input);
+
+  // document.querySelector('.answer').textContent = input;
+  // document.write(`<h1>My name is ${input}</h1>`);
 }
