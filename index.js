f183c32e630d8e9e81b5f4d7b8727bae7bddeb1f
Young Won <youngwon@Youngs-MacBook-Pro.local>
2021-02-28 16:55:40 -0800

    1st commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function othername() {
+  var input = document.getElementById('userInput').value;
+
+  alert(input);
+  document.write(`<h1>My name is ${input}</h1>`);
+}
---
+++ sample.js
@@ -0,0 +1,6 @@
+document.write("<br>this is great <br>");
+alert("Hello");
+var fName = prompt("what is your first name <br>");
+var lName = " Won";
+
+document.write(`my name is ${fName + lName}`);
